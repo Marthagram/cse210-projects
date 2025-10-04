53c01a0cc6ff3746d035bb1ffa3569633906efed
marthagram <mrsmartha.otas@gmail.com>
2025-10-04 22:11:24 +0100

    Homework (Completed Assignment class learning activity)

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment asst1 = new Assignment("Jordan", "Algebra");
+        Console.WriteLine(asst1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Gabbie", "Equation"," 4.2"," 4-5");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeWorkList());
+
+        WritingAssignment write1 = new WritingAssignment("Janet", "Citizenship", "Civic Education in Africa");
+        Console.WriteLine(write1.GetWritingInformation());
+
     }
 }
